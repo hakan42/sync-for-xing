10483b6838942faa2682e222251166da8d97372a
Hakan Tandogan <hakan@gurkensalat.com>
2012-11-22 19:53:30 +0100

    main application Activity starts Accounts overview

--- xing-sync/src/main/java/com/gurkensalat/android/xingsync/Constants.java
+++ xing-sync/src/main/java/com/gurkensalat/android/xingsync/Constants.java
@@ -9,6 +9,11 @@ public class Constants
     public static final String ACCOUNT_TYPE = "com.gurkensalat.android.xingsync";
 
     /**
+     * Provider authority
+     */
+    public static final String PROVIDER_AUTHORITY = "com.gurkensalat.android.xingsync";
+
+    /**
      * Authtoken type string.
      */
     public static final String AUTHTOKEN_TYPE = "com.gurkensalat.android.xingsync";
--- xing-sync/src/main/java/com/gurkensalat/android/xingsync/HelloAndroidActivity.java
+++ xing-sync/src/main/java/com/gurkensalat/android/xingsync/HelloAndroidActivity.java
@@ -1,8 +1,10 @@
 package com.gurkensalat.android.xingsync;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.provider.Settings;
 
 public class HelloAndroidActivity extends Activity
 {
@@ -23,7 +25,15 @@ public class HelloAndroidActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "onCreate");
-        setContentView(R.layout.account_entry);
-    }
+        // setContentView(R.layout.account_entry);
 
+        Intent intent = new Intent(Settings.ACTION_SYNC_SETTINGS);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+
+        // To show only our own accounts
+        intent.putExtra(Settings.EXTRA_AUTHORITIES, new String[]
+        { Constants.PROVIDER_AUTHORITY });
+
+        startActivity(intent);
+    }
 }
