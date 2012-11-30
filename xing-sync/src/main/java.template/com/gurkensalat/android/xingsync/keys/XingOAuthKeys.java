7c51cf4d689444d699610ed73ef9e8419ad15620
Hakan Tandogan <hakan.tandogan@gi-de.com>
2012-11-30 18:28:05 +0100

    Holder for XING OAuth keys

---
+++ xing-sync/src/main/java.template/com/gurkensalat/android/xingsync/keys/XingOAuthKeys.java
@@ -0,0 +1,14 @@
+package com.gurkensalat.android.xingsync.keys;
+
+public class XingOAuthKeys
+{
+    /**
+     * OAuth consumer key. To be obtained from dev.xing.com
+     */
+    public static final String XING_CONSUMER_KEY = "${xingsync.consumer.key}";
+
+    /**
+     * OAuth consumer secret. To be obtained from dev.xing.com
+     */
+    public static final String XING_CONSUMER_SECRET = "${xingsync.consumer.secret}";
+}
