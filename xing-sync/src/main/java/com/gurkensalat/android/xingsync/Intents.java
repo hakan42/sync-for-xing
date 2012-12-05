e642a78d9373cee89bfdc16436c49223d4eb4239
Hakan Tandogan <hakan@gurkensalat.com>
2012-12-05 18:21:14 +0100

    Common place for our intents

---
+++ xing-sync/src/main/java/com/gurkensalat/android/xingsync/Intents.java
@@ -0,0 +1,38 @@
+package com.gurkensalat.android.xingsync;
+
+import android.content.Intent;
+
+/**
+ * Helper for creating intents
+ */
+public class Intents
+{
+    /**
+     * Prefix for all intents created
+     */
+    public static final String INTENT_PREFIX = "com.gurkensalat.android.xingsync.";
+
+    /**
+     * Prefix for all extra data added to intents
+     */
+    public static final String INTENT_EXTRA_PREFIX = INTENT_PREFIX + "extra.";
+
+    /**
+     * Builder for generating an intent configured with extra data such as an
+     * issue, repository, or gist
+     */
+    public static class Builder
+    {
+        private Intent intent;
+
+        /**
+         * Get built intent
+         *
+         * @return intent
+         */
+        public Intent toIntent()
+        {
+            return intent;
+        }
+    }
+}
