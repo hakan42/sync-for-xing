32d9754aafab46c37627c1a9eed7a3c1ad7ad444
Hakan Tandogan <hakan@gurkensalat.com>
2013-10-01 12:54:49 +0200

    Placeholder for OAuth keys and data provider URLs

---
+++ xing-sync/src/main/java.template/com/gurkensalat/android/xingsync/keys/XingOAuthKeys.java
@@ -0,0 +1,59 @@
+package com.gurkensalat.android.xingsync.keys;
+
+public class XingOAuthKeys
+{
+    /**
+     * OAuth consumer key. To be obtained from dev.xing.com
+     */
+    public static final String CONSUMER_KEY          = "${xingsync.consumer.key}";
+
+    /**
+     * OAuth consumer secret. To be obtained from dev.xing.com
+     */
+    public static final String CONSUMER_SECRET       = "${xingsync.consumer.secret}";
+
+    /**
+     * API URL base
+     */
+    public static final String API_URL_BASE         = "${xingapi.url.base}";
+
+    /**
+     * OAuth Request URL. Defined in API documentation at dev.xing.com
+     */
+    public static final String REQUEST_URL           = "${xingapi.request_token_path}";
+
+    /**
+     * OAuth Authorization URL. Defined in API documentation at dev.xing.com
+     */
+    public static final String AUTHORIZE_URL         = "${xingapi.authorize_path}";
+
+    /**
+     * OAuth Token Access URL. Defined in API documentation at dev.xing.com
+     */
+    public static final String ACCESS_URL            = "${xingapi.access_token_path}";
+
+    /**
+     * Blurb.
+     */
+    public static final String OAUTH_CALLBACK_SCHEME = "x-oauthflow";
+
+    /**
+     * Blurb.
+     */
+    public static final String OAUTH_CALLBACK_HOST   = "callback";
+
+    /**
+     * Blurb.
+     */
+    public static final String OAUTH_CALLBACK_URL    = OAUTH_CALLBACK_SCHEME + "://" + OAUTH_CALLBACK_HOST;
+
+    /**
+     * Blurb.
+     */
+    public static final String ENCODING              = "UTF-8";
+
+    /**
+     * Blurb.
+     */
+    public static final String SCOPE                 = "furbl";
+}
