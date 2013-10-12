60cbc012b381f05d33863136ee4f15f70430a354
Hakan Tandogan <hakan@gurkensalat.com>
2013-10-12 22:58:42 +0200

    First use of annotated classes

--- xing-sync-app/src/main/java/com/gurkensalat/android/xingsync/HelloAndroidActivity.java
+++ xing-sync-app/src/main/java/com/gurkensalat/android/xingsync/HelloAndroidActivity.java
@@ -1,25 +1,30 @@
 package com.gurkensalat.android.xingsync;
 
+import com.googlecode.androidannotations.annotations.EActivity;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class HelloAndroidActivity extends Activity {
+@EActivity(R.layout.main)
+public class HelloAndroidActivity extends Activity
+{
+	private static String TAG = "xing-sync";
 
-    private static String TAG = "xing-sync";
-
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after
-     * previously being shut down then this Bundle contains the data it most
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.i(TAG, "onCreate");
-        setContentView(R.layout.main);
-    }
-
+	/**
+	 * Called when the activity is first created.
+	 * 
+	 * @param savedInstanceState
+	 *            If the activity is being re-initialized after previously being
+	 *            shut down then this Bundle contains the data it most recently
+	 *            supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
+	 *            is null.</b>
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		Log.i(TAG, "onCreate");
+		setContentView(R.layout.main);
+	}
 }
-
