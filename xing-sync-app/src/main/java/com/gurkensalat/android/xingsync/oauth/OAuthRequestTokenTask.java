fce1a130a73585cf27db14d4afdfd9e95a37d4c7
Hakan Tandogan <hakan@gurkensalat.com>
2013-10-27 22:52:13 +0100

    Launch browser to perform authentication

---
+++ xing-sync-app/src/main/java/com/gurkensalat/android/xingsync/oauth/OAuthRequestTokenTask.java
@@ -0,0 +1,75 @@
+package com.gurkensalat.android.xingsync.oauth;
+
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.OAuthProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.gurkensalat.android.xingsync.keys.XingOAuthKeys;
+
+/**
+ * An asynchronous task that communicates with Google to retrieve a request
+ * token. (OAuthGetRequestToken)
+ * 
+ * After receiving the request token from Google, show a browser to the user to
+ * authorize the Request Token. (OAuthAuthorizeToken)
+ * 
+ */
+public class OAuthRequestTokenTask extends AsyncTask<Void, Void, Void>
+{
+	/** The tag used to log to adb console. **/
+	private static final String TAG = "xing-sync.OAuthRequestTokenTask";
+
+	private Context context;
+	private OAuthProvider provider;
+	private OAuthConsumer consumer;
+
+	/**
+	 * 
+	 * We pass the OAuth consumer and provider.
+	 * 
+	 * @param context
+	 *            Required to be able to start the intent to launch the browser.
+	 * @param provider
+	 *            The OAuthProvider object
+	 * @param consumer
+	 *            The OAuthConsumer object
+	 */
+	public OAuthRequestTokenTask(Context context, OAuthConsumer consumer, OAuthProvider provider)
+	{
+		this.context = context;
+		this.consumer = consumer;
+		this.provider = provider;
+	}
+
+	/**
+	 * 
+	 * Retrieve the OAuth Request Token and present a browser to the user to
+	 * authorize the token.
+	 * 
+	 */
+	@Override
+	protected Void doInBackground(Void... params)
+	{
+		Log.i(TAG, "doInBackground");
+
+		try
+		{
+			Log.i(TAG, "Retrieving request token from XING servers");
+			final String url = provider.retrieveRequestToken(consumer, XingOAuthKeys.OAUTH_CALLBACK_URL);
+			Log.i(TAG, "Popping a browser with the authorize URL : " + url);
+			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url)).setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP
+			        | Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_FROM_BACKGROUND);
+			context.startActivity(intent);
+		}
+		catch (Exception e)
+		{
+			Log.e(TAG, "Error during OAUth retrieve request token", e);
+		}
+
+		return null;
+	}
+}
--- xing-sync-app/src/main/java/com/gurkensalat/android/xingsync/oauth/PrepareRequestTokenActivity.java
+++ xing-sync-app/src/main/java/com/gurkensalat/android/xingsync/oauth/PrepareRequestTokenActivity.java
@@ -1,24 +1,75 @@
 package com.gurkensalat.android.xingsync.oauth;
 
+import java.net.URLEncoder;
+
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.OAuthProvider;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+import oauth.signpost.commonshttp.CommonsHttpOAuthProvider;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.sharedpreferences.Pref;
+import com.gurkensalat.android.xingsync.SyncPrefs_;
+import com.gurkensalat.android.xingsync.keys.XingOAuthKeys;
 
 @EActivity
 public class PrepareRequestTokenActivity extends Activity
 {
 	private static String TAG = "xing-sync.PrepareRequestTokenActivity";
 
+	private OAuthConsumer consumer;
+	private OAuthProvider provider;
+
+	@Pref
+	SyncPrefs_ syncPrefs;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
+		Log.i(TAG, "onCreate()");
 		super.onCreate(savedInstanceState);
-		// NO VIEW FOR THIS ACTIVITY
-		// setContentView(R.layout.main);
-		Log.i(TAG, "onCreate() called.");
-		finish();
+		try
+		{
+			System.setProperty("debug", "true");
+			this.consumer = new CommonsHttpOAuthConsumer(XingOAuthKeys.CONSUMER_KEY, XingOAuthKeys.CONSUMER_SECRET);
+			this.provider = new CommonsHttpOAuthProvider(XingOAuthKeys.REQUEST_URL + "?scope="
+			        + URLEncoder.encode(XingOAuthKeys.SCOPE, XingOAuthKeys.ENCODING), XingOAuthKeys.ACCESS_URL,
+			        XingOAuthKeys.AUTHORIZE_URL);
+		}
+		catch (Exception e)
+		{
+			Log.e(TAG, "Error creating consumer / provider", e);
+		}
+
+		Log.i(TAG, "Starting task to retrieve request token.");
+		new OAuthRequestTokenTask(this, consumer, provider).execute();
+	}
+
+	/**
+	 * Called when the OAuthRequestTokenTask finishes (user has authorized the
+	 * request token). The callback URL will be intercepted here.
+	 */
+	@Override
+	public void onNewIntent(Intent intent)
+	{
+		Log.i(TAG, "onNewIntent()");
+		super.onNewIntent(intent);
+		final Uri uri = intent.getData();
+		if (uri != null && uri.getScheme().equals(XingOAuthKeys.OAUTH_CALLBACK_SCHEME))
+		{
+			Log.i(TAG, "Callback received : " + uri);
+			Log.i(TAG, "Retrieving Access Token");
+			// new RetrieveAccessTokenTask(this, consumer, provider,
+			// syncPrefs).execute(uri);
+			finish();
+		}
 	}
 
 }
