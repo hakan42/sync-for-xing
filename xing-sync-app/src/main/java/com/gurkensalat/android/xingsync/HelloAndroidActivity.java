c54d0d5bf2cc60117091278c844ffd8aceaa8a34
Hakan Tandogan <hakan@gurkensalat.com>
2013-10-27 23:56:11 +0100

    Display obtained secrets

--- xing-sync-app/src/main/java/com/gurkensalat/android/xingsync/HelloAndroidActivity.java
+++ xing-sync-app/src/main/java/com/gurkensalat/android/xingsync/HelloAndroidActivity.java
@@ -1,18 +1,18 @@
 package com.gurkensalat.android.xingsync;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EActivity;
 import com.googlecode.androidannotations.annotations.FragmentById;
+import com.googlecode.androidannotations.annotations.ViewById;
 import com.googlecode.androidannotations.annotations.sharedpreferences.Pref;
 import com.gurkensalat.android.xingsync.oauth.OAuthButtonsFragment;
 import com.gurkensalat.android.xingsync.oauth.OAuthResultsFragment;
 import com.gurkensalat.android.xingsync.oauth.OAuthSecretsFragment;
-import com.gurkensalat.android.xingsync.oauth.PrepareRequestTokenActivity;
 import com.gurkensalat.android.xingsync.oauth.PrepareRequestTokenActivity_;
 
 @EActivity(R.layout.main)
@@ -31,6 +31,32 @@ public class HelloAndroidActivity extends Activity
 
 	@FragmentById(R.id.main_layout_fragment_oauth_results)
 	OAuthResultsFragment resultsFragment;
+
+	@ViewById(R.id.oauth_token)
+	TextView oauthToken;
+
+	@ViewById(R.id.oauth_token_secret)
+	TextView oauthTokenSecret;
+
+	@Override
+	protected void onResume()
+	{
+		// onResume() is always called just before activity is displayed
+		Log.i(TAG, "onResume()");
+		super.onResume();
+
+		Log.i(TAG, "oauth_token: '" + syncPrefs.oauth_token().get() + "'");
+		if (oauthToken != null)
+		{
+			oauthToken.setText(syncPrefs.oauth_token().get());
+		}
+
+		Log.i(TAG, "oauth_token_secret: '" + syncPrefs.oauth_token_secret().get() + "'");
+		if (oauthTokenSecret != null)
+		{
+			oauthTokenSecret.setText(syncPrefs.oauth_token_secret().get());
+		}
+	}
 
 	@Click(R.id.btn_launch_oauth)
 	void launchOAuth(View clickedView)
