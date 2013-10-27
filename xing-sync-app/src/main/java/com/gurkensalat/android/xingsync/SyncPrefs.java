55dcf3072949df0abc2f1584b188e1aca0761253
Hakan Tandogan <hakan@gurkensalat.com>
2013-10-27 23:09:10 +0100

    A tiny bit of javadoc pointing to the OAuth definitions

--- xing-sync-app/src/main/java/com/gurkensalat/android/xingsync/SyncPrefs.java
+++ xing-sync-app/src/main/java/com/gurkensalat/android/xingsync/SyncPrefs.java
@@ -6,9 +6,15 @@ import com.googlecode.androidannotations.annotations.sharedpreferences.SharedPre
 @SharedPref(value = SharedPref.Scope.UNIQUE)
 public interface SyncPrefs
 {
+	/**
+	 * @see oauth.signpost.OAuth.OAUTH_TOKEN
+	 */
 	@DefaultString("")
 	String oauth_token();
 
+	/**
+	 * @see oauth.signpost.OAuth.OAUTH_TOKEN_SECRET
+	 */
 	@DefaultString("")
 	String oauth_token_secret();
 }
