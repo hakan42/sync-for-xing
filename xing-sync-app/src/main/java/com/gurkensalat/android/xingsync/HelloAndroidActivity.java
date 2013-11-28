ba25452eb729bc239f86ac53376e1712ea0955a9
Hakan Tandogan <hakan@gurkensalat.com>
2013-11-28 23:06:47 +0100

    Demonstrate 'me' call from main screen

--- xing-sync-app/src/main/java/com/gurkensalat/android/xingsync/HelloAndroidActivity.java
+++ xing-sync-app/src/main/java/com/gurkensalat/android/xingsync/HelloAndroidActivity.java
@@ -1,17 +1,36 @@
 package com.gurkensalat.android.xingsync;
 
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
+import com.googlecode.androidannotations.annotations.Bean;
+import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
+import com.googlecode.androidannotations.annotations.sharedpreferences.Pref;
+import com.gurkensalat.android.xingsync.api.MeCall;
+import com.gurkensalat.android.xingsync.api.User;
 
 @EActivity(R.layout.main)
 public class HelloAndroidActivity extends Activity
 {
-	private static String TAG = "xing-sync";
+	private static String TAG = "xingsync.HelloAndroidActivity";
+
+	@ViewById(R.id.oauth_api_call_result)
+	TextView oauth_api_call_result;
+
+	@Bean
+	MeCall meCall;
+
+	@Pref
+	SyncPrefs_ syncPrefs;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -28,4 +47,42 @@ public class HelloAndroidActivity extends Activity
 
 		startActivity(intent);
 	}
+
+	@Click(R.id.btn_perform_me_call)
+	void performMeCall(View clickedView)
+	{
+		Log.i(TAG, "About to call 'me' api method");
+		if (meCall != null)
+		{
+
+			String text = "Call 'Me'";
+			text = text + "\n";
+
+			JSONObject json = meCall.perform();
+			if (json != null)
+			{
+				text = text + json.toString();
+				text = text + "\n";
+
+				User user = User.fromJSON(json);
+				if (user != null)
+				{
+					text = text + user.toString();
+					text = text + "\n";
+
+				}
+			}
+
+			oauth_api_call_result.setText(text);
+		}
+	}
+
+	@Click(R.id.btn_clear_credentials)
+	void clearCredentials(View clickedView)
+	{
+		Log.i(TAG, "About to clear credentials");
+		syncPrefs.oauth_token().put("");
+		syncPrefs.oauth_token_secret().put("");
+	}
+
 }
