c2b295e2053b305c3dd499692866ce9ff2e1c396
Hakan Tandogan <hakan@gurkensalat.com>
2013-12-01 12:24:03 +0100

    #11: Use annotated version of the activity

--- xing-sync-app/src/main/java/com/gurkensalat/android/xingsync/preferences/AccountPreferencesActivity.java
+++ xing-sync-app/src/main/java/com/gurkensalat/android/xingsync/preferences/AccountPreferencesActivity.java
@@ -1,16 +1,44 @@
 package com.gurkensalat.android.xingsync.preferences;
 
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.sharedpreferences.Pref;
 import com.gurkensalat.android.xingsync.R;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.util.Log;
 
+@EActivity
 public class AccountPreferencesActivity extends PreferenceActivity
 {
+	private static final String TAG = "xingsync.AccountPreferencesActivity";
+
+	@Pref
+	SyncPrefs_ syncPrefs;
+
 	@Override
 	public void onCreate(Bundle icicle)
 	{
 		super.onCreate(icicle);
 		addPreferencesFromResource(R.xml.preferences_resources);
+		Log.i(TAG, "onCreate() called");
+	}
+
+	@Override
+	protected void onStart()
+	{
+		Log.i(TAG, "onStart() called");
+
+		super.onStart();
+		// preferences.registerOnSharedPreferenceChangeListener(backupListener);
+	}
+
+	@Override
+	protected void onStop()
+	{
+		Log.i(TAG, "onStop() called");
+
+		// preferences.unregisterOnSharedPreferenceChangeListener(backupListener);
+		super.onStop();
 	}
 }
