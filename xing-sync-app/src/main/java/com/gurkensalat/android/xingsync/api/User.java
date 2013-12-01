9746075c6572a4e2b3d6d913ac43d22bc8225638
Hakan Tandogan <hakan@gurkensalat.com>
2013-12-01 14:41:11 +0100

    Actually parse the JSON stream

--- xing-sync-app/src/main/java/com/gurkensalat/android/xingsync/api/User.java
+++ xing-sync-app/src/main/java/com/gurkensalat/android/xingsync/api/User.java
@@ -1,5 +1,7 @@
 package com.gurkensalat.android.xingsync.api;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class User
@@ -14,13 +16,56 @@ public class User
 
 	public static User fromJSON(JSONObject json)
 	{
-		User u = new User();
+		User u = null;
 
-		// TODO really parse the JSON
-		u.setId("3382304");
-		u.setFirstName("Hakan");
-		u.setLastName("Tandogan");
-		u.setDisplayName(u.getFirstName() + " " + u.getLastName());
+		try
+		{
+			if (json != null)
+			{
+				if (json.has("users"))
+				{
+					JSONArray array = json.getJSONArray("users");
+					if (array.length() > 0)
+					{
+						json = array.getJSONObject(0);
+						System.err.println(json.getClass().getName());
+
+						u = new User();
+
+						if (json.has("id"))
+						{
+							u.setId(json.getString("id"));
+							// XING privacy protection feature :-)
+							int i = u.getId().indexOf("_");
+							if (i > 0)
+							{
+								u.setId(u.getId().substring(0, i));
+							}
+						}
+
+						if (json.has("first_name"))
+						{
+							u.setFirstName(json.getString("first_name"));
+						}
+
+						if (json.has("last_name"))
+						{
+							u.setLastName(json.getString("last_name"));
+						}
+
+						if (json.has("display_name"))
+						{
+							u.setDisplayName(json.getString("display_name"));
+						}
+					}
+				}
+			}
+		}
+		catch (JSONException e)
+		{
+			// TODO move JSONException to signature or use androlog
+			System.err.println(e);
+		}
 
 		return u;
 	}
