7ecd8f9cf413490dffe82871831f4d84d427ac65
Hakan Tandogan <hakan@gurkensalat.com>
2013-12-01 13:42:23 +0100

    Mock Me API call

--- xing-sync-app/src/main/java/com/gurkensalat/android/xingsync/api/MeCall.java
+++ xing-sync-app/src/main/java/com/gurkensalat/android/xingsync/api/MeCall.java
@@ -6,41 +6,53 @@ import org.json.JSONObject;
 import android.util.Log;
 
 import com.googlecode.androidannotations.annotations.EBean;
+import com.googlecode.androidannotations.annotations.sharedpreferences.Pref;
+import com.gurkensalat.android.xingsync.preferences.SyncPrefs_;
 
 @EBean
 public class MeCall // implements XingApiCall
 {
 	private static final String TAG = "xingsync.MeCall";
 
+	@Pref
+	SyncPrefs_ syncPrefs;
+
 	public JSONObject perform(final Object... args)
 	{
-		// Hardcoded Mock for now
-
-		StringBuffer sb = new StringBuffer(1024);
-
-		sb.append("{");
-		sb.append("  \"users\": [");
-		sb.append("    {");
-		sb.append("      \"id\": \"3382304_64b174\",");
-		sb.append("      \"first_name\": \"Hakan\",");
-		sb.append("      \"last_name\": \"Tandogan\",");
-		sb.append("      \"display_name\": \"Hakan Tandogan\"");
-		sb.append("    }");
-		sb.append("  ]");
-		sb.append("}");
-
-		Log.i(TAG, sb.toString());
-
 		JSONObject json = new JSONObject();
-		try
+
+		if (syncPrefs.debugMockApiCalls().get())
 		{
-			json = new JSONObject(sb.toString());
-			Log.i(TAG, json.toString());
+			StringBuffer sb = new StringBuffer(1024);
+
+			sb.append("{");
+			sb.append("  \"users\": [");
+			sb.append("    {");
+			sb.append("      \"id\": \"3382304_64b174\",");
+			sb.append("      \"first_name\": \"Hakan\",");
+			sb.append("      \"last_name\": \"Tandogan\",");
+			sb.append("      \"display_name\": \"Hakan Tandogan\"");
+			sb.append("    }");
+			sb.append("  ]");
+			sb.append("}");
+
+			Log.i(TAG, sb.toString());
+
+			try
+			{
+				json = new JSONObject(sb.toString());
+			}
+			catch (JSONException e)
+			{
+				Log.e(TAG, "while parsing answer string", e);
+			}
 		}
-		catch (JSONException e)
+		else
 		{
-			Log.e(TAG, "while parsing answer string", e);
+			Log.i(TAG, "Hardcoded Mock for now");
 		}
+
+		// Log.i(TAG, json.toString());
 
 		return json;
 	}
