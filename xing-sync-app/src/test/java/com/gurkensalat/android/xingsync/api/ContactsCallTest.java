1331d273ea67562e0ac736062473114a25aaf7b8
Hakan Tandogan <hakan@gurkensalat.com>
2013-12-11 14:16:17 +0100

    Load more extensive test data from classpath

--- xing-sync-app/src/test/java/com/gurkensalat/android/xingsync/api/ContactsCallTest.java
+++ xing-sync-app/src/test/java/com/gurkensalat/android/xingsync/api/ContactsCallTest.java
@@ -4,8 +4,12 @@ import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.List;
 
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Before;
@@ -30,40 +34,14 @@ public class ContactsCallTest
 	}
 
 	@Before
-	public void populateJson() throws JSONException
+	public void populateJson() throws JSONException, IOException
 	{
-		StringBuffer sb = new StringBuffer(1024);
+		// JSON is fetched from logcat and reformatted with
+		// http://jsonformatter.curiousconcept.com/
 
-		sb.append("{");
-		sb.append("  \"contacts\": {");
-		sb.append("    \"total\": 297,");
-		sb.append("    \"users\": [");
-		sb.append("      {");
-		sb.append("        \"id\": \"6841253_007b9e\",");
-		sb.append("        \"first_name\": \"Hasmet\",");
-		sb.append("        \"last_name\": \"Acar\",");
-		sb.append("        \"display_name\": \"Hasmet Acar\",");
-		sb.append("        \"active_email\": null");
-		sb.append("      },");
-		sb.append("      {");
-		sb.append("        \"id\": \"4261890_ffd916\",");
-		sb.append("        \"first_name\": \"Murat\",");
-		sb.append("        \"last_name\": \"Acun\",");
-		sb.append("        \"display_name\": \"Murat Acun\",");
-		sb.append("        \"active_email\": null");
-		sb.append("      },");
-		sb.append("      {");
-		sb.append("        \"id\": \"12824542_5078cc\",");
-		sb.append("        \"first_name\": \"Petra\",");
-		sb.append("        \"last_name\": \"Ghotra\",");
-		sb.append("        \"display_name\": \"Petra Ghotra\",");
-		sb.append("        \"active_email\": null");
-		sb.append("      }");
-		sb.append("    ]");
-		sb.append("  }");
-		sb.append("}");
+		String data = IOUtils.toString(getClass().getResourceAsStream("ContactsCallTest.json"), Charsets.UTF_8);
 
-		JSONObject actual = new JSONObject(sb.toString());
+		JSONObject actual = new JSONObject(data);
 		assertThat("JSON object could not be created", actual, is(notNullValue()));
 
 		json = actual;
@@ -75,18 +53,14 @@ public class ContactsCallTest
 		List<User> actual = testable.parse(json);
 
 		assertThat("Users list could not be created", actual, is(notNullValue()));
-		assertThat("Three users would have to be parsed", actual.size(), is(3));
+		assertThat("Two users would have to be parsed", actual.size(), is(2));
 
-		assertThat("Id #0 not correctly parsed", actual.get(0).getId(), is("6841253"));
-		assertThat("First Name #0 not correctly parsed", actual.get(0).getFirstName(), is("Hasmet"));
-		assertThat("Last Name #0 not correctly parsed", actual.get(0).getLastName(), is("Acar"));
+		assertThat("Id #0 not correctly parsed", actual.get(0).getId(), is("3382304"));
+		assertThat("First Name #0 not correctly parsed", actual.get(0).getFirstName(), is("Hakan"));
+		assertThat("Last Name #0 not correctly parsed", actual.get(0).getLastName(), is("Tandogan"));
 
-		assertThat("Id #1 not correctly parsed", actual.get(1).getId(), is("4261890"));
-		assertThat("First Name #1 not correctly parsed", actual.get(1).getFirstName(), is("Murat"));
-		assertThat("Last Name #1 not correctly parsed", actual.get(1).getLastName(), is("Acun"));
-
-		assertThat("Id #2 not correctly parsed", actual.get(2).getId(), is("12824542"));
-		assertThat("First Name #2 not correctly parsed", actual.get(2).getFirstName(), is("Petra"));
-		assertThat("Last Name #2 not correctly parsed", actual.get(2).getLastName(), is("Ghotra"));
+		assertThat("Id #1 not correctly parsed", actual.get(1).getId(), is("10244846"));
+		assertThat("First Name #1 not correctly parsed", actual.get(1).getFirstName(), is("Frohwalt"));
+		assertThat("Last Name #1 not correctly parsed", actual.get(1).getLastName(), is("Egerer"));
 	}
 }
