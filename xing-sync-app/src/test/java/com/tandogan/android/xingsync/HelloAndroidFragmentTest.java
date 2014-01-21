6a1fa9473c97b19aa3113fb2e98587e86fba924c
Hakan Tandogan <hakan@gurkensalat.com>
2014-01-21 15:58:54 +0100

    basic kickoff point for robolectric testing

---
+++ xing-sync-app/src/test/java/com/tandogan/android/xingsync/HelloAndroidFragmentTest.java
@@ -0,0 +1,17 @@
+package com.tandogan.android.xingsync;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@Config(manifest = "./target/filtered-manifest/AndroidManifest.xml")
+@RunWith(RobolectricTestRunner.class)
+public class HelloAndroidFragmentTest
+{
+	@Test
+	public void foo()
+	{
+
+	}
+}
