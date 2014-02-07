5259a27dd39cdfa6f105f8155b03f659a7ad3936
Hakan Tandogan <hakan@gurkensalat.com>
2014-02-07 14:42:09 +0100

    Remove deprecated 'is' matcher

--- xing-sync-app/src/test/java/com/tandogan/android/xingsync/api/ContactTest.java
+++ xing-sync-app/src/test/java/com/tandogan/android/xingsync/api/ContactTest.java
@@ -1,6 +1,5 @@
 package com.tandogan.android.xingsync.api;
 
-import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
@@ -32,7 +31,7 @@ public class ContactTest
 		String data = IOUtils.toString(getClass().getResourceAsStream("ContactTest.json"), Charsets.UTF_8);
 
 		JSONObject actual = new JSONObject(data);
-		assertThat("JSON object could not be created", actual, is(notNullValue()));
+		assertThat("JSON object could not be created", actual, notNullValue());
 
 		json = actual;
 	}
@@ -41,21 +40,21 @@ public class ContactTest
 	public void parseMeCall() throws JSONException
 	{
 		Contact actual = Contact.fromJSON(json);
-		assertThat("User could not be created", actual, is(notNullValue()));
-		assertThat("Id not correctly parsed", actual.getId(), is("3382304"));
-		assertThat("First Name not correctly parsed", actual.getFirstName(), is("Hakan"));
-		assertThat("Last Name not correctly parsed", actual.getLastName(), is("Tandogan"));
-		assertThat("Display Name not correctly parsed", actual.getDisplayName(), is("Hakan Tandogan"));
+		assertThat("User could not be created", actual, notNullValue());
+		assertThat("Id not correctly parsed", actual.getId(), equalTo("3382304"));
+		assertThat("First Name not correctly parsed", actual.getFirstName(), equalTo("Hakan"));
+		assertThat("Last Name not correctly parsed", actual.getLastName(), equalTo("Tandogan"));
+		assertThat("Display Name not correctly parsed", actual.getDisplayName(), equalTo("Hakan Tandogan"));
 	}
 
 	@Test
 	public void data() throws JSONException
 	{
 		Contact actual = Contact.fromJSON(json);
-		assertThat("User could not be created", actual, is(notNullValue()));
+		assertThat("User could not be created", actual, notNullValue());
 
 		actual.setData(ContactsContract.CommonDataKinds.Email.ADDRESS, "someone@somewhere.com");
 		assertThat("Email Address could not be set", actual.getData(ContactsContract.CommonDataKinds.Email.ADDRESS),
-		        is(equalTo("someone@somewhere.com")));
+		        (equalTo("someone@somewhere.com")));
 	}
 }
